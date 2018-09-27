c8aea8754d9bc40a62aad08d65dba9eb6a8e1570
Camilo Chavez <darkcamil@hotmial.com>
2018-09-27 09:46:42 -0500

    Session IV

---
+++ Tricky/Board.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tricky
+{
+    class Board
+    {
+        private char[,] places;
+        private Players players;
+
+        public Board(int height, int width)
+        {
+            places = new char[height, width];
+            players = new Players();
+        }
+
+        public void PrintBoard()
+        {
+         
+            for (int y = 0; y < places.GetLength(0); y++)
+            {
+                for (int x = 0; x < places.GetLength(0); x++)
+                {
+                    Console.Write(" _ ");
+                }
+                Console.WriteLine("");
+                for (int x = 0; x < places.GetLength(0); x++)
+                {
+                    Console.Write("|" + players.ActualPlayer + "|");
+                    players.NextPlayer();
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ Tricky/Players .cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tricky
+{
+    class Players
+    {
+        char playerO;
+        char playerX;
+        char actualPlayer;
+
+        public Players()
+        {
+            playerO = 'O';
+            playerX = 'X';
+            actualPlayer = 'X';
+        }
+
+        public char ActualPlayer
+        {
+            get
+            {
+                return actualPlayer;
+            }
+        }
+        public void NextPlayer()
+        {
+            if (actualPlayer == 'X')
+            {
+                actualPlayer = playerO;
+            }
+            else
+            {
+                actualPlayer = playerX;
+            }
+        }
+    }
+}
